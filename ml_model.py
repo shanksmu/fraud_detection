e9a5a12c34767d9269f4a985dbf70116fcfef97a
shanksmu <shanksmu@gmail.com>
2021-10-26 09:51:22 +0530

    added functions for ML model1, model2 and model3

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all ml related code here")
+
+def model1():
+    print("added code for ML Model1")
+
+def model2():
+    print("added code for ML Model2")
+
+def model3():
+    print("added code for ML Model3")
